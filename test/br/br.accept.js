ee10641d9d8a0731e3f3e13b3cd8d8bda93925ec
Tamnil Saito Junior <tamnil@gmail.com>
2019-01-18 12:13:49 -0200

    chai tests and tests added

--- test/br/br.accept.js
+++ test/br/br.accept.js
@@ -1,4 +1,6 @@
 const assert = require('assert')
+const should = require('chai').should
+const expect = require('chai').expect
 
 const faker = require('../../index.js')
 
@@ -6,17 +8,23 @@ const faker = require('../../index.js')
 describe('hello world',() => {
 
 
-    it('hello world',()=>{
+    it('Should return ',()=>{
 
-let test = [];
-for(let i =1 ; i <10; i++){
-        console.log(faker.br.cpf())
-        console.log(faker.br.cnpj({mask:false}))
-    // test.push(faker.br.cnpj())
-    
 
-}
-console.log(test)
+        let test = [];
+        for(let i =1 ; i <10; i++){
+            // console.log(faker.br.cpf())
+            console.log(faker.br.cnpj({mask:false}))
+            // test.push(faker.br.cnpj())
+
+            expect(faker.br.cpf().length).to.be.equal(11)
+            expect(faker.br.cpf()).to.be.an('string')
+            console.log(faker.br.cpf({format:true}) )
+            console.log(faker.br.cnpj({format:true}) )
+
+        }
+
+        console.log(test)
         assert(true)
     })
 
